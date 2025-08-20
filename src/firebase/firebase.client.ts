9c515281311dacf06503dcb945a61d38dc341da7
Jung Woon Jik <dnswlrsla@gmail.com>
2025-08-20 12:56:35 +0900

    debug: service worker

--- src/firebase/firebase.client.ts
+++ src/firebase/firebase.client.ts
@@ -11,14 +11,28 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+console.log("FIREBASE CONFIG", firebaseConfig);
 
-let messaging: Messaging | undefined = undefined;
-if (typeof window !== "undefined" && typeof window.navigator !== "undefined") {
-  try {
-    messaging = getMessaging(app);
-  } catch (e) {
-    messaging = undefined;
-  }
+// let messaging: Messaging | undefined = undefined;
+// if (typeof window !== "undefined" && typeof window.navigator !== "undefined") {
+//   try {
+//     messaging = getMessaging(app);
+//   } catch (e) {
+//     messaging = undefined;
+//   }
+// }
+
+let messaging: Messaging | undefined;
+
+if (typeof window !== "undefined") {
+  import("firebase/messaging").then(() => {
+    try {
+      messaging = getMessaging(app);
+    } catch (e) {
+      console.error("FCM init failed", e);
+      messaging = undefined;
+    }
+  });
 }
 
 export { messaging };
